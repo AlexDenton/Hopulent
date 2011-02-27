bfc63b0573ea54a7735ee3b10f9a938314c6df32
didier <didier@didier-VirtualBox.(none)>
2011-02-27 00:17:56 -0500

    Updated some of browse features to be in table format. Not finished.

--- browse/views.py
+++ browse/views.py
@@ -6,11 +6,14 @@ from style.models import Style
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
-    return render_to_response('browse/index.html')
+    list_of_latest_beers = Beer.objects.all().order_by('last_mod')[:3]
+    return render_to_response('browse/index.html', {'list_of_latest_beers':
+list_of_latest_beers})
     
 def beers(request):
     list_of_beers = Beer.objects.all().order_by('name')
-    return render_to_response('browse/beers.html', {'list_of_beers': list_of_beers})
+    list_of_breweries = Brewery.objects.all().order_by('name')
+    return render_to_response('browse/beers.html', {'list_of_beers': list_of_beers, 'list_of_breweries': list_of_breweries})
     
 def breweries(request):
      list_of_breweries = Brewery.objects.all().order_by('name')
