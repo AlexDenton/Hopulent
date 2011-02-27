1e5ee220588a4c3af2fa34633d0119ec5f5447e9
didier <didier@didier-VirtualBox.(none)>
2011-02-27 17:39:52 -0500

    Added ratings, brewery, and other header fields

--- browse/views.py
+++ browse/views.py
@@ -19,8 +19,35 @@ def breweries(request):
      list_of_breweries = Brewery.objects.all().order_by('name')
      return render_to_response('browse/breweries.html', {'list_of_breweries': list_of_breweries})
 
+def brewerydetail(request, brewery_id):
+    brewery = get_object_or_404(Brewery, pk=brewery_id)
+    list_of_beers = Beer.objects.filter(brewery_id=brewery.id)
+    return render_to_response('browse/brewerydetail.html', {'brewery': brewery, 'list_of_beers': list_of_beers})
+
 def styles(request):
     list_of_styles = Style.objects.all().order_by('style_name')
     return render_to_response('browse/styles.html', {'list_of_styles': list_of_styles})
 
-	
+def styledetail(request, style_id):
+	style = get_object_or_404(Style, pk=style_id)
+	list_of_beers = Beer.objects.filter(style_id=style.id)
+	return render_to_response('browse/styledetail.html', {'style': style, 'list_of_beers':
+list_of_beers})
+
+def categories(request):
+	list_of_categories = Category.objects.all().order_by('cat_name')
+	return render_to_response('browse/categories.html', {'list_of_categories':
+list_of_categories})
+
+def location(request):
+	list_of_locations = Brewery.objects.all().order_by('state')
+	return render_to_response('browse/location.html', {'list_of_locations':
+list_of_locations})
+
+def locationdetail(request, state):
+	brewery = get_object_or_404(Brewery, pk=state)
+	list_of_breweries = Brewery.objects.filter(state=brewery.state)
+	for brewery in list_of_breweries:
+		list_of_beers = Beer.objects.filter(brewery_id=brewery.id)
+	return render_to_response('browse/locationdetail.html', {'list_of_beers':
+list_of_beers})
