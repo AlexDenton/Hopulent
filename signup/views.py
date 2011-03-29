6e677b03e128228232c2787c52c6982d9534904b
Alfredo <User@.(none)>
2011-03-29 13:15:15 -0400

    signup link

--- signup/views.py
+++ signup/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def index(request):
+def signup(request):
         return render_to_response('signup/signup.html', context_instance=RequestContext(request))
-
