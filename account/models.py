640073bb821f98a4b672870013feb5119573b325
Daniel Sprowls <daniel@daniel-laptop.(none)>
2011-03-29 23:57:07 -0400

    git add <account/models.py> git add <templates/browse/styledetail.html> git add <templates/browse/index.html> git add <templates/browse/breweries.html> git add <templates/browse/beers.html> git add <templates/account/userprofile.html>

--- account/models.py
+++ account/models.py
@@ -5,6 +5,10 @@ from django import forms
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     age = models.IntegerField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    birthday = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
     
  User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
  
