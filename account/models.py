53345d39db0d8d662b912b44cf7a64f9c319c0ec
Daniel Sprowls <daniel@daniel-laptop.(none)>
2011-03-29 21:39:46 -0400

    git add <account/models.py> git add <account/views.py> git add <templates/account/userprofile.html>

--- account/models.py
+++ account/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 
-# class UserProfile(models.Model):
-#    user = models.ForeignKey(User, unique=True)
-#    age = models.IntegerField()
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    age = models.IntegerField()
     
-# User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+ User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
  
 	
